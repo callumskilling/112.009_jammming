e62e9e00f5c970c43966ed8a2b75a600684e3a97
callumskilling <88871549+callumskilling@users.noreply.github.com>
2022-09-20 18:54:45 +1000

    added SearchResults component and styles, added constructor method to App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,15 +6,20 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 export class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { searchResults : ['Running Up That Hill', 'Placebo', 'Covers', '6RHHbAbyIcPpvS1dP3KUGq']}
+  }
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          {/* <!-- Add a SearchBar component --> */}
+          <SearchBar />
           <div className="App-playlist">
-            {/* <!-- Add a SearchResults component --> */}
-            {/* <!-- Add a Playlist component --> */}
+          <SearchResults searchResult = { this.state.searchResults }/>
+          <Playlist />
           </div>
         </div>
       </div>
