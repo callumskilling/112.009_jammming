8ce57cdd284c8c72c451159883ad660eddec4c26
callumskilling <88871549+callumskilling@users.noreply.github.com>
2022-10-07 12:50:49 +1100

    trying to fix warnings preventing netlify from deploying, removed useless constructor and gave <iframe> a title

--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -2,14 +2,9 @@ import React from 'react';
 import './Player.css'
 
 export class Player extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-
     render() {
         return(
-            <iframe src= {this.props.previewTrackUrl} frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <iframe title = "Player" src= {this.props.previewTrackUrl} frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         )
     }
 }
